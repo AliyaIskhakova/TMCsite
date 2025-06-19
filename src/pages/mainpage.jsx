24231d8eed4160180617f7fdebbd1cb85939ad07
AliyaIskhakova <aliya_iskhakova12@mail.ru>
2025-06-19 11:14:37 +0300

    Чистка кода

--- src/pages/mainpage.jsx
+++ src/pages/mainpage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Container, Button, Modal, Form, Row, Col, Card, InputGroup, Alert } from 'react-bootstrap';
-import InputMask from 'react-input-mask'; // Импорт библиотеки для маски ввода
+import InputMask from 'react-input-mask'; 
 
 import { AddRequest } from '../api/requests';
 import { Link } from 'react-router-dom';
